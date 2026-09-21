32b139836cae4d0d806e91e6e2cc4e779fb181b6
VaszocsikLevi <158279338+VaszocsikLevi@users.noreply.github.com>
2026-09-21 20:16:52 +0200

    Add files via upload

---
+++ verseny/script.js
@@ -0,0 +1,42 @@
+const gombok = document.querySelectorAll("#szinpad button");
+const intro = document.getElementById("intro");
+const gyuru = document.getElementById("gyuru");
+const szoveg = document.getElementById("szoveg");
+const mi = [
+  { nev: "Kovács Norbert", szoveg: "ajfbaeufaeu" },
+  { nev: "Hege Domonkos", szoveg: "bvwoivbewbv" },
+  { nev: "Vaszócsik Levente", szoveg: "avj wsjkvsj vsjk vd" }
+];
+
+
+gombok.forEach(function (gomb) {
+  gomb.addEventListener("click", function () {
+
+    gombok.forEach(function (masik) {
+      masik.classList.remove("kivalasztott");
+      masik.classList.add("halvany");
+    });
+
+    gomb.classList.remove("halvany");
+    gomb.classList.add("kivalasztott");
+
+    const i = Number(gomb.dataset.kep);
+    const jobb = (i + 1) % 3;
+    const bal = (i + 2) % 3;
+    const valaki = mi[i];
+    szoveg.innerHTML = `<h3>${valaki.nev}</h3><p>${valaki.szoveg}</p>`;
+
+    gombok.forEach(function (g) {
+    g.classList.remove("bal", "kozep", "jobb");
+    });
+
+    gombok[i].classList.add("kozep");
+    gombok[jobb].classList.add("jobb");
+    gombok[bal].classList.add("bal");
+
+    console.log("Kép:", gomb.dataset.kep);
+    intro.classList.add("felcsuszott");
+    gyuru.classList.add("lathato");
+  });
+});
+
