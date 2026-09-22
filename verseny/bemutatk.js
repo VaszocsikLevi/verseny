74fb9c7519f3e98c4e813a110d3cddb50b86081a
Vaszócsik Levente <vaszocsik.levi@gmail.com>
2026-09-22 19:53:43 +0200

    kepvi

--- verseny/bemutatk.js
+++ verseny/bemutatk.js
@@ -28,7 +28,7 @@ function Kiir() {
                 <li>Homolya Zoltán a felkészítő tanárunk</li>
             </ul>
             
-            <img src="img/IMG_7783.JPG" alt="Apritok">
+            <img src="" alt="Apritok">
             <button id="galeriaGomb">Galéria</button>
         </div>
     </header>
